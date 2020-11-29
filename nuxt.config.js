416d456e0c3732e38f9c87c4b37eb8244704ba90
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-29 07:54:09 +0100

    Indented files with prettier and eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,17 +9,17 @@ export default {
 			{ charset: 'utf-8' },
 			{
 				name: 'viewport',
-				content: 'width=device-width, initial-scale=1',
+				content: 'width=device-width, initial-scale=1'
 			},
-			{ hid: 'description', name: 'description', content: '' },
+			{ hid: 'description', name: 'description', content: '' }
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
 	},
 
 	// Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [],
 	styleResources: {
-		scss: ['@/assets/scss/styles.scss'],
+		scss: ['@/assets/scss/styles.scss']
 	},
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -31,7 +31,7 @@ export default {
 	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
 	buildModules: [
 		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/eslint-module',
+		'@nuxtjs/eslint-module'
 	],
 
 	// Modules (https://go.nuxtjs.dev/config-modules)
@@ -48,11 +48,11 @@ export default {
 				options: {
 					// Configuration options:
 					// https://github.com/jkphl/svg-sprite#configuration-basics
-				},
-			},
-		],
+				}
+			}
+		]
 	],
 
 	// Build Configuration (https://go.nuxtjs.dev/config-build)
-	build: {},
+	build: {}
 }
