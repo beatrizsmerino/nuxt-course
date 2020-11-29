e1ec335b4f919b3a4b6e7b8c28cae67896574c7e
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-29 07:50:21 +0100

    Added config prettier and eslint for vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,48 @@ module.exports = {
 	root: true,
 	env: {
 		browser: true,
-		node: true,
+		node: true
 	},
 	parserOptions: {
-		parser: 'babel-eslint',
+		parser: 'babel-eslint'
 	},
 	extends: [
 		'@nuxtjs',
 		'prettier',
 		'prettier/vue',
-		'plugin:prettier/recommended',
+		'eslint:recommended',
+		'plugin:vue/recommended',
 		'plugin:nuxt/recommended',
+		'plugin:json/recommended'
 	],
 	plugins: ['prettier'],
 	// add your custom rules here
-	rules: {},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				alignAttributesVertically: true,
+				ignores: []
+			}
+		],
+		'vue/html-closing-bracket-newline': [
+			'error',
+			{
+				singleline: 'never',
+				multiline: 'always'
+			}
+		],
+		'vue/singleline-html-element-content-newline': [
+			'error',
+			{
+				ignoreWhenEmpty: true
+			}
+		]
+	}
 }
