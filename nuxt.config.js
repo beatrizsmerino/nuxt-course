d841016414e393d07541efb6ed7f39df9f9842f9
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-29 04:27:52 +0100

    Added npm package for use scss/sass for 'assets' folder nuxt
    Global styles (Share variables, mixins, functions across all style files)

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
 
 	// Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [],
+	styleResources: {
+		scss: ['@/assets/scss/styles.scss'],
+	},
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
 	plugins: [],
@@ -32,7 +35,9 @@ export default {
 	],
 
 	// Modules (https://go.nuxtjs.dev/config-modules)
-	modules: [],
+	modules: [
+		'@nuxtjs/style-resources'
+	],
 
 	// Build Configuration (https://go.nuxtjs.dev/config-build)
 	build: {},
