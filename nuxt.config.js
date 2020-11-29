11ec5d968dd8317c187557a48e797dd4cf025921
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-29 05:50:43 +0100

    Added npm package for use axios in nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,8 @@ export default {
 
 	// Modules (https://go.nuxtjs.dev/config-modules)
 	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
 		'@nuxtjs/style-resources',
 		[
 			'nuxt-svg-sprite-module',
