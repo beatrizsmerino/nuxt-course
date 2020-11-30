09c1e939642a2d336076ff63323259b8a9c69e2d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-30 02:17:30 +0100

    Add two links to the /products page. The links should go to /products/1 and /products/2

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,24 @@
 <template>
-	<h1>
-		Products
-	</h1>
+	<div class="container">
+		<h1 class="title">
+			Products
+		</h1>
+
+		<div class="products-list">
+			<ul>
+				<li>
+					<nuxt-link to="/products/1">
+						Product 1
+					</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to="/products/2">
+						Product 2
+					</nuxt-link>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -11,4 +28,38 @@
 </script>
 
 <style lang="scss">
+	.products-list {
+		width: 100%;
+
+		ul {
+			width: 100%;
+			padding: 0;
+			display: flex;
+			justify-content: center;
+			list-style: none;
+
+			li {
+				width: calc(50% - 2rem);
+				margin: 1rem;
+
+				a {
+					width: 100%;
+					height: 15rem;
+					padding: 2rem;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					text-decoration: none;
+					font-size: 2rem;
+					font-weight: bold;
+					color: $color-brand-3;
+					background-color: mix(#fff, $color-brand-2, 90%);
+
+					&:hover {
+						background-color: mix(#fff, $color-brand-2, 80%);
+					}
+				}
+			}
+		}
+	}
 </style>
