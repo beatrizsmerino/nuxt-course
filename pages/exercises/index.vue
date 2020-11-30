15141547f68597025922ad8142a79a7de3ef39f1
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-11-30 12:49:17 +0100

    Init exercise posts

--- pages/exercises/index.vue
+++ pages/exercises/index.vue
@@ -54,6 +54,13 @@
 						description:
 							'<div class="exercise-steps"><ol><li>Create two routes: /products and /products/:pid</li><li>Add a button to the main page. The button should navigate (programmatically) to /products</li><li>Add two links to the /products page. The links should go to /products/1 and /products/2</li><li>Output the product id on the /products/:pid page</li></ol></div>',
 						status: false
+					},
+					{
+						url: '/exercises/posts',
+						name: 'Posts',
+						description:
+							'<div class="exercise-steps"><ol><li></li></ol></div>',
+						status: false
 					}
 				]
 			};
---
+++ pages/exercises/posts/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container inner">
+		<Title>Posts</Title>
+	</div>
+</template>
+
+<script>
+	export default {
+		layout: 'exercises'
+	};
+</script>
+
+<style lang="scss">
+</style>
