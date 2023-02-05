2dcf024369b663f507cfcd3911fe3c30aeee78a5
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-05 19:24:46 +0100

    BUGFIX. Nuxt app does not running with the `npm run dev` command
    Error:
    - Error message on the VSC terminal:
    ✖ Nuxt Fatal Error│TypeError: Cannot read property 'plugins' of null
    - Github Actions:
    https://github.com/beatrizsmerino/nuxt-exercises/actions/runs/4094585560
    
    Solution:
    - Remove all files and replace it by new instalation obtained from the next repository
    https://github.com/beatrizsmerino/interview-technical-aptitude-test/commit/ac9a4121d08b9703246739e1af6a769afb3cbe83
    
    Description:
    - 1. Installation of Nuxt v.2.
    Creating a project with the Vue framework, Nuxt, in its version 2.
    
    Steps:
    - Created Nuxt App
    create-nuxt-app v5.0.0
    ✨  Generating Nuxt.js project in nuxt-app
    ? Project name: nuxt-app
    ? Programming language: JavaScript
    ? Package manager: Npm
    ? UI framework: None
    ? Template engine: HTML
    ? Nuxt.js modules: Axios - Promise based HTTP client
    ? Linting tools: ESLint, Prettier, StyleLint
    ? Testing framework: Jest
    ? Rendering mode: Universal (SSR / SSG)
    ? Deployment target: Server (Node.js hosting)
    ? Development tools: jsconfig.json (Recommended for VS Code if you're not using typescript), Semantic Pull Requests, Dependabot (For auto-updating dependencies, GitHub only)
    ? Continuous integration: GitHub Actions (GitHub only)
    ? What is your GitHub username? beatriz sopeña merino
    ? Version control system: Git

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,76 +1,48 @@
 export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-app',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
 
-	// Target (https://go.nuxtjs.dev/config-target)
-	"target": "static",
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
 
-	// Global page headers (https://go.nuxtjs.dev/config-head)
-	"head": {
-		"title": "nuxt-app",
-		"meta": [
-			{
-				"charset": "utf-8",
-			},
-			{
-				"name": "viewport",
-				"content": "width=device-width, initial-scale=1",
-			},
-			{
-				"hid": "description",
-				"name": "description",
-				"content": "",
-			},
-		],
-		"link": [
-			{
-				"rel": "icon",
-				"type": "image/x-icon",
-				"href": "/favicon.ico",
-			},
-		],
-	},
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-	// Global CSS (https://go.nuxtjs.dev/config-css)
-	"css": [],
-	"styleResources": {
-		"scss": [
-			"@/assets/scss/styles.scss",
-		],
-	},
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-	"plugins": [],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
 
-	// Auto import components (https://go.nuxtjs.dev/config-components)
-	"components": true,
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
 
-	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-	"buildModules": [
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
 
-		// https://go.nuxtjs.dev/eslint
-		[
-			"@nuxtjs/eslint-module",
-			{
-				"fix": true,
-			},
-		],
-		"@nuxtjs/svg-sprite",
-	],
-	"svgSprite": {
-
-		// Manipulate module options
-		"input": "~/assets/images/icons/",
-	},
-
-	// Modules (https://go.nuxtjs.dev/config-modules)
-	"modules": [
-
-		// https://go.nuxtjs.dev/axios
-		"@nuxtjs/axios",
-		"@nuxtjs/style-resources",
-	],
-
-	// Build Configuration (https://go.nuxtjs.dev/config-build)
-	"build": {
-		"postcss": null,
-	},
-};
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
