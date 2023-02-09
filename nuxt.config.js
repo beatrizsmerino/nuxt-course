47525caa845d5e4816d6682bd1cce49041de340e
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-09 17:38:27 +0100

    REFACTOR. Move the configuration form `axios` into nuxt modules
    Steps:
    - The `axios` module is added via the line `@nuxtjs/axios`, which indicates that the official `axios` module for `Nuxt.js` is being used.
    - After adding the module, a configuration object is provided that contains a single property: `baseURL`. The `baseURL` property is used to set the base URL that will be used to make HTTP requests. In this case, it is set to `'/'`, which means that the domain root will be used for requests. If no `baseURL` is provided, a default base URL of `http://localhost:3000` will be used.

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,8 +33,15 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    [
+      // https://go.nuxtjs.dev/axios
+      '@nuxtjs/axios',
+      {
+        // Axios module configuration: https://go.nuxtjs.dev/config-axios
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/',
+      },
+    ],
     [
       // https://www.npmjs.com/package/@nuxtjs/style-resources
       '@nuxtjs/style-resources',
@@ -43,12 +50,6 @@ export default {
       },
     ],
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
