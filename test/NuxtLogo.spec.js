e75aaee80bd4b6ce586560111afb406ae50eb52c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-09 19:55:01 +0100

    REFACTOR. Rename `NuxtLogo` to `LogoNuxt` component

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import LogoNuxt from '@/components/LogoNuxt.vue'
 
-describe('NuxtLogo', () => {
+describe('LogoNuxt', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(LogoNuxt)
     expect(wrapper.vm).toBeTruthy()
   })
 })
