bb4d8345c3353a1b8f82d7ea150f5c2b20dfa6f0
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-09 18:39:35 +0100

    SPRITE. Add svg files to check that the sprite works.
    Steps:
    - Create the `about` page, to test new things in a clean file.
    - Use the `svg-icon` component of the `@nuxtjs/svg-sprite` module.
    - Add the svg files `logo-nuxt.js` and `logo-vue.js`.
    - Add styles for side-by-side logo placement

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="logo__list">
+      <svg-icon class="logo" name="logo-vue-js" />
+      <svg-icon class="logo" name="logo-nuxt-js" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 10rem;
+  height: 10rem;
+
+  &__list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
