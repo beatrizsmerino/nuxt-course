064be5c9cc14645890dbe66aa47fdc01408ff7e9
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-16 18:47:16 +0100

    REFACTOR. Create `ButtonLink` dynamic component
    Steps:
    - Create `ButtonLink.vue` file.
    - Import and use `ButtonLink` component inside `ButtonList`.
    - Move the styles of `button` from `ButtonList` to `ButtonLink` component.
    - Use a dynamic component to use different types of html tags with the button styles: `nuxt-link`, `a` or `button`.
    - Use the `buttonLink` prop for send the data of the `name`, `path`, `to`... to the dynamic component.

---
+++ components/Button/ButtonLink.vue
@@ -0,0 +1,69 @@
+<template>
+  <component
+    :is="getTypeTag"
+    :class="`button ${buttonLink.modifier}`"
+    :href="buttonLink.path"
+    :to="buttonLink.to"
+    :target="!buttonLink.to ? `_blank` : ''"
+    :rel="!buttonLink.to ? 'noopener noreferrer' : ''"
+  >
+    {{ buttonLink.name }}
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'ButtonLink',
+  props: {
+    buttonLink: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    getTypeTag() {
+      if (this.buttonLink.path) {
+        return 'a'
+      }
+
+      if (this.buttonLink.to) {
+        return 'nuxt-link'
+      }
+
+      return 'button'
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: inline-block;
+  padding: 0.5rem 2rem;
+  border-width: 0.2rem;
+  border-style: solid;
+  border-radius: 0.4rem;
+  font-size: 1rem;
+  text-decoration: none;
+
+  &--green {
+    border-color: $color-brand-1;
+    color: $color-brand-1;
+
+    &:hover {
+      background-color: $color-brand-1;
+      color: $color-white;
+    }
+  }
+
+  &--grey {
+    border-color: $color-brand-2;
+    color: $color-brand-2;
+
+    &:hover {
+      background-color: $color-brand-2;
+      color: $color-white;
+    }
+  }
+}
+</style>
--- components/Button/ButtonList.vue
+++ components/Button/ButtonList.vue
@@ -1,21 +1,19 @@
 <template>
   <ul class="button-list">
     <li v-for="button in buttonList" :key="button" class="button-list__item">
-      <a
-        :href="button.path"
-        target="_blank"
-        rel="noopener noreferrer"
-        :class="`button ${button.modifier}`"
-      >
-        {{ button.name }}
-      </a>
+      <ButtonLink :button-link="button" />
     </li>
   </ul>
 </template>
 
 <script>
+import ButtonLink from '@/components/Button/ButtonLink'
+
 export default {
   name: 'ButtonList',
+  components: {
+    ButtonLink,
+  },
   props: {
     buttonList: {
       type: Array,
@@ -37,36 +35,6 @@ export default {
   &__item {
     &:not(:last-child) {
       margin-right: 1.5rem;
-    }
-  }
-}
-
-.button {
-  display: inline-block;
-  padding: 0.5rem 2rem;
-  border-width: 0.2rem;
-  border-style: solid;
-  border-radius: 0.4rem;
-  font-size: 1rem;
-  text-decoration: none;
-
-  &--green {
-    border-color: $color-brand-1;
-    color: $color-brand-1;
-
-    &:hover {
-      background-color: $color-brand-1;
-      color: $color-white;
-    }
-  }
-
-  &--grey {
-    border-color: $color-brand-2;
-    color: $color-brand-2;
-
-    &:hover {
-      background-color: $color-brand-2;
-      color: $color-white;
     }
   }
 }
