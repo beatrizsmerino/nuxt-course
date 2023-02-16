38bdfc37a2326c81d0d5c646fca771166ac5b2f8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-16 18:17:18 +0100

    REFACTOR. Create `ButtonList` component
    Steps:
    - Create the `ButtonList.vue` file.
    - Move the `Documentation` and `Github` buttons from the `about` page to the new `ButtonList` component.
    - Use the `buttonList` prop in the `about` page to send the `name`, `path` and `modifier` data to the new `ButtonList` component.

---
+++ components/Button/ButtonList.vue
@@ -0,0 +1,73 @@
+<template>
+  <ul class="button-list">
+    <li v-for="button in buttonList" :key="button" class="button-list__item">
+      <a
+        :href="button.path"
+        target="_blank"
+        rel="noopener noreferrer"
+        :class="`button ${button.modifier}`"
+      >
+        {{ button.name }}
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ButtonList',
+  props: {
+    buttonList: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+  &__item {
+    &:not(:last-child) {
+      margin-right: 1.5rem;
+    }
+  }
+}
+
+.button {
+  display: inline-block;
+  padding: 0.5rem 2rem;
+  border-width: 0.2rem;
+  border-style: solid;
+  border-radius: 0.4rem;
+  font-size: 1rem;
+  text-decoration: none;
+
+  &--green {
+    border-color: $color-brand-1;
+    color: $color-brand-1;
+
+    &:hover {
+      background-color: $color-brand-1;
+      color: $color-white;
+    }
+  }
+
+  &--grey {
+    border-color: $color-brand-2;
+    color: $color-brand-2;
+
+    &:hover {
+      background-color: $color-brand-2;
+      color: $color-white;
+    }
+  }
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -4,28 +4,7 @@
       <article>
         <LogoList :logo-list="logoList" />
         <h2 class="title">nuxt-app</h2>
-        <ul class="button__list">
-          <li class="button__item">
-            <a
-              href="https://nuxtjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="button button--green"
-            >
-              Documentation
-            </a>
-          </li>
-          <li class="button__item">
-            <a
-              href="https://github.com/nuxt/nuxt.js"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="button button--grey"
-            >
-              GitHub
-            </a>
-          </li>
-        </ul>
+        <ButtonList :button-list="buttonList" />
       </article>
     </section>
   </div>
@@ -33,11 +12,13 @@
 
 <script>
 import LogoList from '@/components/Logo/LogoList'
+import ButtonList from '@/components/Button/ButtonList'
 
 export default {
   name: 'About',
   components: {
     LogoList,
+    ButtonList,
   },
   data() {
     return {
@@ -47,6 +28,18 @@ export default {
         },
         {
           name: 'logo-nuxt-js',
+        },
+      ],
+      buttonList: [
+        {
+          name: 'Documentation',
+          path: 'https://nuxtjs.org/',
+          modifier: 'button--green',
+        },
+        {
+          name: 'GitHub',
+          path: 'https://github.com/nuxt/nuxt.js',
+          modifier: 'button--grey',
         },
       ],
     }
@@ -64,50 +57,5 @@ export default {
   font-weight: 300;
   letter-spacing: 0.1rem;
   text-align: center;
-}
-
-.button {
-  display: inline-block;
-  padding: 0.5rem 2rem;
-  border-width: 0.2rem;
-  border-style: solid;
-  border-radius: 0.4rem;
-  font-size: 1rem;
-  text-decoration: none;
-
-  &__list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-  &__item {
-    &:not(:last-child) {
-      margin-right: 1.5rem;
-    }
-  }
-
-  &--green {
-    border-color: $color-brand-1;
-    color: $color-brand-1;
-
-    &:hover {
-      background-color: $color-brand-1;
-      color: $color-white;
-    }
-  }
-
-  &--grey {
-    border-color: $color-brand-2;
-    color: $color-brand-2;
-
-    &:hover {
-      background-color: $color-brand-2;
-      color: $color-white;
-    }
-  }
 }
 </style>
