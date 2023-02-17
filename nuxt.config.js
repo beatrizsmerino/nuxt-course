6877da6ed2af6a4727d55027c0395628ed5b8b7f
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-17 01:36:18 +0100

    CONFIG. Add `eslint` and `stylelint` to nuxt configuration
    Steps to follow:
    - Add configuration in `buildModules` to `@nuxtjs/eslint-module` and `@nuxtjs/stylelint-module`, for `lint` and `format` code.

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,11 +25,24 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
-
+    [
+      // https://go.nuxtjs.dev/eslint
+      '@nuxtjs/eslint-module',
+      {
+        fix: true,
+        cache: true,
+        emitWarning: true,
+      },
+    ],
+    [
+      // https://go.nuxtjs.dev/stylelint
+      '@nuxtjs/stylelint-module',
+      {
+        fix: true,
+        cache: true,
+        emitWarning: true,
+      },
+    ],
     [
       // https://www.npmjs.com/package/@nuxtjs/svg-sprite
       '@nuxtjs/svg-sprite',
