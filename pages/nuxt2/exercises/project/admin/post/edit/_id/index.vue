63cf0f06944e36c0641f3aa455f546cdd977827c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-04 18:25:23 +0100

    feat(project): create new `Edit post` page using `PostForm` component

---
+++ pages/nuxt2/exercises/project/admin/post/edit/_id/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<Layout class="layout-admin">
+		<template #title>
+			Edit post #{{ $route.params.id }}
+		</template>
+		<template #content>
+			<PostForm />
+		</template>
+	</Layout>
+</template>
+
+<script>
+	import Layout from "@/components/Nuxt2/Exercises/Project/Layout";
+	import PostForm from "@/components/Nuxt2/Exercises/Project/Post/PostForm";
+
+	export default {
+		"name": "ViewProjectAdminPostEdit",
+		"components": {
+			Layout,
+			PostForm,
+		},
+		"layout": "exercises",
+	};
+</script>
