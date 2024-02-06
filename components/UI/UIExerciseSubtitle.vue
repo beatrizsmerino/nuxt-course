84df65f94e5090ec810e37f068eb439fede99e5b
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-06 17:16:51 +0100

    refactor(layout): rename `LayoutTitle` and `LayoutSubtitle` components
    Steps:
    - Rename from `LayoutTitle` to `UIExerciseTitle`.
    - Rename from `LayoutSubtitle` to `UIExerciseSubtitle`.

--- components/UI/UIExerciseSubtitle.vue
+++ components/UI/UIExerciseSubtitle.vue
@@ -6,7 +6,7 @@
 
 <script>
 	export default {
-		"name": "LayoutSubtitle",
+		"name": "UIExerciseSubtitle",
 	};
 </script>
 
--- components/UI/UIExerciseTitle.vue
+++ components/UI/UIExerciseTitle.vue
@@ -6,7 +6,7 @@
 
 <script>
 	export default {
-		"name": "LayoutTitle",
+		"name": "UIExerciseTitle",
 	};
 </script>
 
