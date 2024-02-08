a6ef05bf0ac9163486ac112196c378f31c67fc37
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-08 03:58:55 +0100

    feat(project): move the `fetch` data to the `vuex` store

--- pages/nuxt2/exercises/project/posts/index.vue
+++ pages/nuxt2/exercises/project/posts/index.vue
@@ -15,7 +15,6 @@
 </template>
 
 <script>
-	import postListData from "~/assets/data/data-post-list.json";
 	import Layout from "@/components/Nuxt2/Exercises/Project/Layout/Layout";
 	import PostList from "@/components/Nuxt2/Exercises/Project/Post/PostList";
 
@@ -26,26 +25,6 @@
 			PostList,
 		},
 		"layout": "exercises",
-		fetch(context) {
-			if (context.store.state.postList.length > 0) {
-				return null;
-			}
-
-			return new Promise((resolve, reject) => {
-				// eslint-disable-next-line nuxt/no-timing-in-fetch-data
-				setTimeout(() => {
-					resolve({
-						"postList": postListData,
-					});
-				}, 1000);
-			})
-				.then(data => {
-					context.store.commit("setPostList", data.postList);
-				})
-				.catch(error => {
-					context.error(error);
-				});
-		},
 		"computed": {
 			getPostList() {
 				return this.$store.getters.getPostList;
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import postListData from "~/assets/data/data-post-list.json";
 
 // eslint-disable-next-line import/no-named-as-default-member
 const createStore = () => new Vuex.Store({
@@ -11,6 +12,20 @@ const createStore = () => new Vuex.Store({
 		},
 	},
 	"actions": {
+		fetchPostList(vuexContext, context) {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					vuexContext.commit("setPostList", postListData);
+					resolve();
+				}, 1000);
+			})
+				.then(data => {
+					context.store.commit("setPostList", data.postList);
+				})
+				.catch(error => {
+					context.error(error);
+				});
+		},
 		setPostList(vuexContext, data) {
 			vuexContext.commit("setPostList", data);
 		},
