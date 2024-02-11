689c614a4e11f4beeede059ab78dd22069b760b5
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-11 17:22:15 +0100

    refactor(project): use `set` prefix for `mutations`

--- store/index.js
+++ store/index.js
@@ -9,23 +9,23 @@ const createStore = () => new Store({
 		"isError": null,
 	},
 	"mutations": {
-		readPostList(state, data) {
+		setReadPostList(state, data) {
 			state.postList = data;
 		},
-		readPostSelected(state, data) {
+		setReadPostSelected(state, data) {
 			state.postSelected = data;
 		},
-		createPost(state, data) {
+		setCreatePost(state, data) {
 			state.postList.push(data);
 		},
-		updatePost(state, data) {
+		setUpdatePost(state, data) {
 			const postIndex = state.postList.findIndex(post => post.id === data.id);
 			state.postList[postIndex] = data;
 		},
-		deletePost(state, id) {
+		setDeletePost(state, id) {
 			state.postList = state.postList.filter(post => post.id !== id);
 		},
-		createError(state, error) {
+		setCreateError(state, error) {
 			state.isError = error;
 		},
 	},
@@ -41,20 +41,20 @@ const createStore = () => new Store({
 							...response.data[key],
 						});
 					}
-					vuexContext.commit("readPostList", postList);
+					vuexContext.commit("setReadPostList", postList);
 				})
 				.catch(error => {
-					vuexContext.commit("createError", error);
+					vuexContext.commit("setCreateError", error);
 				});
 		},
 		readPostList(vuexContext, data) {
-			vuexContext.commit("readPostList", data);
+			vuexContext.commit("setReadPostList", data);
 		},
 		readPostSelected(vuexContext, id) {
 			return axios
 				.get(`https://nuxt-course-b5643-default-rtdb.firebaseio.com/posts/${id}.json`)
 				.then(result => {
-					vuexContext.commit("readPostSelected", result.data);
+					vuexContext.commit("setReadPostSelected", result.data);
 				});
 		},
 		createPost(vuexContext, data) {
@@ -72,7 +72,7 @@ const createStore = () => new Store({
 						.then(resultUpdated => {
 							console.log(resultUpdated);
 
-							vuexContext.commit("createPost", {
+							vuexContext.commit("setCreatePost", {
 								...data,
 								"id": resultUpdated.data.name,
 							});
@@ -89,7 +89,7 @@ const createStore = () => new Store({
 				.then(result => {
 					console.log(result);
 
-					vuexContext.commit("updatePost", data);
+					vuexContext.commit("setUpdatePost", data);
 				})
 				.catch(error => console.log(error));
 		},
@@ -97,7 +97,7 @@ const createStore = () => new Store({
 			return axios
 				.delete(`https://nuxt-course-b5643-default-rtdb.firebaseio.com/posts/${id}.json`)
 				.then(() => {
-					vuexContext.commit("deletePost", id);
+					vuexContext.commit("setDeletePost", id);
 				})
 				.catch(error => console.error(error));
 		},
