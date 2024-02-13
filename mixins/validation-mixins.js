49d156f268080b663b4a655630cd365a8f9eb5e2
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-13 02:51:54 +0100

    feat(project): create `isUrl` function in plugins folder

--- mixins/validation-mixins.js
+++ mixins/validation-mixins.js
@@ -1,3 +1,16 @@
 export const isString = (data) => typeof data === "string";
 export const isEmpty = (data) => !data.trim();
 export const isMaxLength = (data, numberCharacters) => data.length < numberCharacters;
+export const isUrl = (data) => {
+	if (!isEmpty(data)) {
+		try {
+			new URL(data);
+
+			return true;
+		} catch {
+			return false;
+		}
+	}
+
+	return true;
+};
