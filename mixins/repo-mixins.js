9be7a73a83ab5a13eca1461b416e265b5e5cbf4d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-15 19:25:42 +0100

    feat(config): set `title` of `head` in `nuxt.config` using `name` property of `package.json`
    Steps:
    - Create helper functions `removeKebabCase` and `addUppercaseFirstLetter` in `helper-functions-mixins.js` file.
    - Import `package.json` and `helper-functions-mixins.js` files in the `repo-mixins.js`.
    - Get the `name` property of the `package.json` file to get the `nuxt-course` text.
    - Use the `removeKebabCase` and `addUppercaseFirstLetter` functions to transform the `nuxt-course` text into `Nuxt course`.
    
    Reason:
    - This way when the project name is changed in the `package.json` file it will be modified in all parts of the project.
    
    References:
    - https://v2.nuxt.com/docs/configuration-glossary/configuration-head/
    - https://adrianalonso.es/cajon-desatre/convencion-de-nombres-desde-el-camelcase-hasta-el-kebab-case/
    - https://www.freecodecamp.org/news/javascript-capitalize-first-letter-of-word/

--- mixins/repo-mixins.js
+++ mixins/repo-mixins.js
@@ -1,1 +1,4 @@
-export const getRepoName = () => "Nuxt Course";
+import pkg from "../package.json";
+import { removeKebabCase, addUppercaseFirstLetter } from "./helper-functions-mixins.js";
+
+export const getRepoName = () => addUppercaseFirstLetter(removeKebabCase(pkg.name));
