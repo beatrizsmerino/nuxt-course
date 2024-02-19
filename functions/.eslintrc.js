f2d6d8e40b6836b82d9647f64fefb2bc31a0f64c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-19 17:54:26 +0100

    feat(config): configures the `eslintrc` and `index.js` files in the `functions` folder
    Steps:
    - In the `functions/index.js` file copy and paste the code of the link:
    https://medium.com/@sirofjelly/deploying-a-nuxt-ssr-server-side-rendering-app-to-google-firebase-5d90117167db#8925
    - There are 2 `eslint` configurations in the project, one in the root and the other inside the `functions` folder.
    So extend the root configuration to the `functions` folder, so you don't have to duplicate it in both places.
    
    References:
    - https://medium.com/@sirofjelly/deploying-a-nuxt-ssr-server-side-rendering-app-to-google-firebase-5d90117167db
    - https://medium.com/likecoin/quick-nuxt-js-ssr-prototyping-with-firebase-cloud-functions-5277553610a8

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
-  extends: ["eslint:recommended", "google"],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
-    },
-  ],
-  globals: {},
+	env: {
+		es6: true,
+		node: true,
+	},
+	parserOptions: {
+		"ecmaVersion": 2018,
+	},
+	extends: [".eslintrc", "eslint:recommended", "google"],
+	rules: {
+		"no-restricted-globals": ["error", "name", "length"],
+		"prefer-arrow-callback": "error",
+		"quotes": ["error", "double", { "allowTemplateLiterals": true }],
+	},
+	overrides: [
+		{
+			files: ["**/*.spec.*"],
+			env: {
+				mocha: true,
+			},
+			rules: {},
+		},
+	],
+	globals: {},
 };
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// index.js
+import functions from "firebase-functions";
+import { Nuxt } from "nuxt-start";
+import nuxtConfig from "./nuxt.config.js";
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const config = {
+	...nuxtConfig,
+	dev: false,
+	buildDir: "nuxt",
+};
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const nuxt = new Nuxt(config);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const ssrapp = functions.https.onRequest(async (req, res) => {
+	await nuxt.ready();
+	nuxt.render(req, res);
+});
