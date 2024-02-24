34fb6ea576477f0204ae203ac285f0563a5dcaab
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-02-24 02:13:40 +0100

    feat(project): add validation of required fields to `AuthForm`
    Steps:
    - Add `field-error-text` prop to each `FormItem`, to send an error message inside the component.
    - Add `@blur` event, to validate the fields individually with `validateEmail` and `validatePassword` methods.
    - Adds the `validationErrors` object to the vue data.
    - Adds the `isValid` computed property, that checks for any error message before doing signIn/signOut.
    - Imports and uses the `validateFieldString` and `validateFieldRequired` mixins.
    - The `validateEmail` and `validatePassword` methods, use the `validateFieldString` and `validateFieldRequired` mixins to validate the fields and generate the error messages.
    - Add the `updateErrorText` method, to control the reactivity of the data vue `validationErrors`.
    - Add `validateForm` method to trigger all field validations at the same time when submitting the form.
    - In the `onSubmit` method, it executes the validation of all the fields with `validationForm`, checks if they are valid with the `isValid` computed property and if so uses the `$emit` to send the data to the parent component.

--- components/Exercises/Project/Auth/AuthForm.vue
+++ components/Exercises/Project/Auth/AuthForm.vue
@@ -12,6 +12,8 @@
 				field-type="text"
 				field-modifier="anim"
 				:field-required="true"
+				:field-error-text="validationErrors.email"
+				@blur="validateEmail"
 				@update:fieldValue="value => (form.email = value)"
 			/>
 			<FormItem
@@ -21,6 +23,8 @@
 				field-type="password"
 				field-modifier="anim"
 				:field-required="true"
+				:field-error-text="validationErrors.password"
+				@blur="validatePassword"
 				@update:fieldValue="value => (form.password = value)"
 			/>
 			<div class="form__button-list">
@@ -44,6 +48,7 @@
 </template>
 
 <script>
+	import { validateFieldString, validateFieldRequired } from "@/mixins/validation-mixins.js";
 	import FormItem from "@/components/Exercises/Project/Form/FormItem";
 
 	export default {
@@ -58,14 +63,46 @@
 					"email": "",
 					"password": "",
 				},
+				"validationErrors": {},
 			};
 		},
+		"computed": {
+			isValid() {
+				return Object.keys(this.validationErrors).length === 0;
+			},
+		},
 		"methods": {
+			updateErrorText(fieldName, errorMessage) {
+				if (errorMessage) {
+					this.$set(this.validationErrors, fieldName, errorMessage);
+				} else {
+					this.$delete(this.validationErrors, fieldName);
+				}
+			},
+			validateEmail() {
+				const errorText = validateFieldString(this.form.email) || validateFieldRequired(this.form.email);
+
+				this.updateErrorText("email", errorText);
+			},
+			validatePassword() {
+				const errorText = validateFieldString(this.form.password) || validateFieldRequired(this.form.password);
+
+				this.updateErrorText("password", errorText);
+			},
+			validateForm() {
+				this.validationErrors = {};
+
+				this.validateEmail();
+				this.validatePassword();
+			},
 			onSubmit() {
-				this.$emit("submit:auth", {
-					...this.form,
-					"isSignIn": this.isSignIn,
-				});
+				this.validateForm();
+				if (this.isValid) {
+					this.$emit("submit:auth", {
+						...this.form,
+						"isSignIn": this.isSignIn,
+					});
+				}
 			},
 			onSwitch() {
 				this.isSignIn = !this.isSignIn;
