34539626afe61c3c1948df135a3034a8265f2a42
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-10-21 04:05:48 +0200

    fix(package): annoying experimental warning about import JSON when running `dev` command
    Error:
    - (node:46971) ExperimentalWarning: Importing JSON modules is an experimental feature and might change at any time
    (Use `node --trace-warnings ...` to show where the warning was created)
    
    Reason:
    - In the `mixins/repo-mixins.js` file, `import pkg from "../package.json" assert { type: "json"};` is being used.
    This is an experimental feature in Node.js to import JSON files using ECMAScript module syntax (ESM). Node.js has not yet stabilised this functionality, so it issues a warning that you are using an experimental feature.
    
    Solution:
    - Use the alternative approach with `fs` to avoid the warning: If you prefer to avoid the warning and be more compatible with future versions, you can read the JSON file using the `fs` module and then parse it.
    This approach uses the `fs.readFile()` function to read the JSON file and then converts it into an object with `JSON.parse()`. It also uses `import.meta.url` to get the relative URL of the JSON file, which is necessary when working with ECMAScript modules.
    ```
    import { promises as fs } from "fs";
    const packageJson = JSON.parse(await fs.readFile(new URL('../package.json', import.meta.url)));
    ```

--- mixins/repo-mixins.js
+++ mixins/repo-mixins.js
@@ -1,5 +1,7 @@
-import pkg from "../package.json" assert { type: "json" };
+import { promises as fs } from "fs";
 import { removeKebabCase, addUppercaseFirstLetter } from "./helper-functions-mixins.js";
+
+const pkg = JSON.parse(await fs.readFile(new URL("../package.json", import.meta.url)));
 
 export const getRepoName = () => addUppercaseFirstLetter(removeKebabCase(pkg.name) || "");
 export const getRepoDescription = () => pkg.description || "";
